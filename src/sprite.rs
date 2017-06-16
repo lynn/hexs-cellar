3ef87091e92a6c5fc9b1d98643f5d7aca227ee22
Lynn <lynn@foldr.moe>
2017-06-17 01:38:55 +0200

    Transcribe monster data

--- src/sprite.rs
+++ src/sprite.rs
@@ -1,4 +1,9 @@
-type Color = u8;
+pub enum Color {
+    Navy, Green, Teal, Maroon,
+    Purple, Brown, Gray, Dark,
+    Blue, Lime, Aqua, Red,
+    Pink, Yellow, White,
+}
 
 pub struct Sprite {
     pub character: char,
